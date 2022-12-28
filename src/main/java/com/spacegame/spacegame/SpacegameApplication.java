09ef923d3797b032936308986ad7f1980aafbaa5
Sam Skinner <sam.sjskinner@gmail.com>
2022-12-28 12:09:54 +1100

    init spring

---
+++ src/main/java/com/spacegame/spacegame/SpacegameApplication.java
@@ -0,0 +1,13 @@
+package com.spacegame.spacegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacegameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacegameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spacegame/spacegame/SpacegameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacegame.spacegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacegameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
