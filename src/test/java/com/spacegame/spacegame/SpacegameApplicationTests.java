69700416f3bedeb44a3a0a76bf0ec4ac7c256c6b
Sam Skinner <sam.sjskinner@gmail.com>
2023-02-03 20:29:22 +1100

    update cf infra

--- src/test/java/com/spacegame/spacegame/SpacegameApplicationTests.java
+++ src/test/java/com/spacegame/spacegame/SpacegameApplicationTests.java
@@ -2,12 +2,14 @@ package com.spacegame.spacegame;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class SpacegameApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.isTrue(true);
 	}
 
 }
